d7602687884e76935cb80e8c64c9bb5407681f30
Troy Melhase <troy@monster.ax>
2023-08-30 11:06:11 -0400

    wip.

--- metrics.go
+++ metrics.go
@@ -4,14 +4,26 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	// "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func MakeCounter(name string, help string) prometheus.Counter {
+func MakeCounter(namespace string, subsystem string, name string, help string, constLabels prometheus.Labels) prometheus.Counter {
 	return promauto.NewCounter(prometheus.CounterOpts{
-		Name: name,
-		Help: help,
+		Namespace:   namespace,
+		Subsystem:   subsystem,
+		Name:        name,
+		Help:        help,
+		ConstLabels: constLabels,
 	})
+}
+
+func MakeCounterVec(namespace string, subsystem string, name string, help string, constLabels prometheus.Labels, labelNames []string) *prometheus.CounterVec {
+	return promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace:   namespace,
+		Subsystem:   subsystem,
+		Name:        name,
+		Help:        help,
+		ConstLabels: constLabels,
+	}, labelNames)
 }
 
 var MetricsHandler = promhttp.Handler
