328ba0b6524f0e30db3403ae331f1b9dc2b795ed
Troy Melhase <troy@monster.ax>
2023-08-31 15:05:27 -0400

    wip.

--- metrics.go
+++ metrics.go
@@ -6,7 +6,16 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func MakeCounter(namespace string, subsystem string, name string, help string, constLabels prometheus.Labels) prometheus.Counter {
+var (
+	MetricsHandler = promhttp.Handler
+)
+
+type (
+	MetricLabels     = prometheus.Labels
+	MetricLabelNames = []string
+)
+
+func MakeCounter(namespace string, subsystem string, name string, help string, constLabels MetricLabels) prometheus.Counter {
 	return promauto.NewCounter(prometheus.CounterOpts{
 		Namespace:   namespace,
 		Subsystem:   subsystem,
@@ -16,7 +25,17 @@ func MakeCounter(namespace string, subsystem string, name string, help string, c
 	})
 }
 
-func MakeCounterVec(namespace string, subsystem string, name string, help string, constLabels prometheus.Labels, labelNames []string) *prometheus.CounterVec {
+func MakeCounterFunc(namespace string, subsystem string, name string, help string, constLabels MetricLabels, fn func() float64) prometheus.CounterFunc {
+	return promauto.NewCounterFunc(prometheus.CounterOpts{
+		Namespace:   namespace,
+		Subsystem:   subsystem,
+		Name:        name,
+		Help:        help,
+		ConstLabels: constLabels,
+	}, fn)
+}
+
+func MakeCounterVec(namespace string, subsystem string, name string, help string, constLabels MetricLabels, labelNames MetricLabelNames) *prometheus.CounterVec {
 	return promauto.NewCounterVec(prometheus.CounterOpts{
 		Namespace:   namespace,
 		Subsystem:   subsystem,
@@ -26,4 +45,82 @@ func MakeCounterVec(namespace string, subsystem string, name string, help string
 	}, labelNames)
 }
 
-var MetricsHandler = promhttp.Handler
+func MakeGauge(namespace string, subsystem string, name string, help string, constLabels MetricLabels) prometheus.Gauge {
+	return promauto.NewGauge(prometheus.GaugeOpts{
+		Namespace:   name,
+		Subsystem:   subsystem,
+		Name:        name,
+		Help:        help,
+		ConstLabels: constLabels,
+	})
+}
+
+func MakeGaugeFunc(namespace string, subsystem string, name string, help string, constLabels MetricLabels, fn func() float64) prometheus.GaugeFunc {
+	return promauto.NewGaugeFunc(prometheus.GaugeOpts{
+		Namespace:   namespace,
+		Subsystem:   subsystem,
+		Name:        name,
+		Help:        help,
+		ConstLabels: constLabels,
+	}, fn)
+}
+
+func MakeGaugeVec(namespace string, subsystem string, name string, help string, constLabels MetricLabels, labelNames MetricLabelNames) *prometheus.GaugeVec {
+	return promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace:   name,
+		Subsystem:   subsystem,
+		Name:        name,
+		Help:        help,
+		ConstLabels: constLabels,
+	}, labelNames)
+}
+
+func MakeHistogram(namespace string, subsystem string, name string, help string, constLabels MetricLabels) prometheus.Histogram {
+	return promauto.NewHistogram(prometheus.HistogramOpts{
+		Namespace:   name,
+		Subsystem:   subsystem,
+		Name:        name,
+		Help:        help,
+		ConstLabels: constLabels,
+	})
+}
+
+func MakeHistogramVec(namespace string, subsystem string, name string, help string, constLabels MetricLabels, labelNames MetricLabelNames) *prometheus.HistogramVec {
+	return promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace:   name,
+		Subsystem:   subsystem,
+		Name:        name,
+		Help:        help,
+		ConstLabels: constLabels,
+	}, labelNames)
+}
+
+func MakeSummary(namespace string, subsystem string, name string, help string, constLabels MetricLabels) prometheus.Summary {
+	return promauto.NewSummary(prometheus.SummaryOpts{
+		Namespace:   name,
+		Subsystem:   subsystem,
+		Name:        name,
+		Help:        help,
+		ConstLabels: constLabels,
+	})
+}
+
+func MakeSummaryVec(namespace string, subsystem string, name string, help string, constLabels MetricLabels, labelNames MetricLabelNames) *prometheus.SummaryVec {
+	return promauto.NewSummaryVec(prometheus.SummaryOpts{
+		Namespace:   name,
+		Subsystem:   subsystem,
+		Name:        name,
+		Help:        help,
+		ConstLabels: constLabels,
+	}, labelNames)
+}
+
+func MakeUntypedFunc(namespace string, subsystem string, name string, help string, constLabels MetricLabels, fn func() float64) prometheus.UntypedFunc {
+	return promauto.NewUntypedFunc(prometheus.UntypedOpts{
+		Namespace:   name,
+		Subsystem:   subsystem,
+		Name:        name,
+		Help:        help,
+		ConstLabels: constLabels,
+	}, fn)
+}
