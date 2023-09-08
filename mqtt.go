f50d7dd472764569cb60db791e24218175d5fdc5
Troy Melhase <troy@monster.ax>
2023-09-08 17:23:47 -0500

    wip.

--- mqtt.go
+++ mqtt.go
@@ -3,14 +3,16 @@ package gooseneck
 import mqtt "github.com/eclipse/paho.mqtt.golang"
 
 const (
-	BROKER = "BROKER"
-	TOPIC  = "TOPIC"
+	MQTT_BROKER = "MQTT_BROKER"
+	MQTT_TOPIC  = "MQTT_TOPIC"
 )
 
-func NewMessageQueueClient(broker string, clientId string) mqtt.Client {
+func NewMessageQueueClient(broker string, clientId string, username string, password string) mqtt.Client {
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker(broker)
 	opts.SetClientID(clientId)
+	opts.SetUsername(username)
+	opts.SetPassword(password)
 	opts.OnConnect = func(client mqtt.Client) {
 		Info().Str("broker", broker).Str("client", clientId).Msg("connected")
 	}
